7a878fab943ab26c257f1db0ad6d8e3feb1c5e50
Maigre <thomas.bohl@gmail.com>
2015-10-06 10:59:16 +0200

    Startup message

--- jdj-server.js
+++ jdj-server.js
@@ -1,4 +1,8 @@
 
+// CONFIG
+var PORT_WS = 8088;
+var PORT_PUB = 8081;
+var PORT_TIME = 8082;
 
 // LIBS
 var Servers = require('./servers');
@@ -14,15 +18,15 @@ var STATE = {
 var STATEOBSERVER = new Servers.Observer(STATE, function(c) { REMOTECTRL.sendStatus(STATE) });
 
   // TIME SERVER
-var TIMESERVER = new Servers.TimeServer(8082);
+var TIMESERVER = new Servers.TimeServer(PORT_TIME);
 
   // PUBLISHER
-var PUBLISHER = new Servers.Publisher(8081);
+var PUBLISHER = new Servers.Publisher(PORT_PUB);
 PUBLISHER.onSubscribe = function(fd, ep) { STATE.clientCount++; }
 PUBLISHER.onUnsubscribe = function(fd, ep) { STATE.clientCount--; }
 
 // CONTROLLER
-var REMOTECTRL = new Servers.RemoteCtrl(8088);
+var REMOTECTRL = new Servers.RemoteCtrl(PORT_WS);
 REMOTECTRL.onConnect = function(client) { STATE.controllerCount++ };
 REMOTECTRL.onDisconnect = function(client) { STATE.controllerCount-- };
 REMOTECTRL.onHello = function(client) {
@@ -30,3 +34,7 @@ REMOTECTRL.onHello = function(client) {
   client.emit('status', { hello: 'you' });
   PUBLISHER.send("Hello everyone !");
 };
+
+
+var ip = require( 'os' ).networkInterfaces( ).eth0[0].address;
+console.log("Server Ready - "+ip+" - WS: "+PORT_WS+" - PUB: "+PORT_PUB+" - TIME: "+PORT_TIME);
