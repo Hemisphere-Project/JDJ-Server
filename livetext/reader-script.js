b90bfba9cc25da38483a5dac752f12172a99e9b5
Jeremie <jeremie@hemisphere-project.com>
2015-11-24 17:46:09 +0100

    scroll when size change

--- livetext/reader-script.js
+++ livetext/reader-script.js
@@ -2,64 +2,70 @@
 $(function() {
 
 
-    var text='temp text ceci est un texte essai';
-    var progressServer = 0;
+  var text='temp text ceci est un texte essai';
+  var progressServer = 0;
 
 
-    buildText();
+  buildText();
 
-    function buildText(){
-      $("#visuText").empty();
-      var textArray = text.split('');
-      $.each(textArray, function(index,char){
-        var charDiv = $('<span>').addClass("singleChar untyped").html(char).appendTo($("#visuText"));
-      });
-    }
-
-
-    function actuVisu(){
-      $('.singleChar').each(function(key, charDiv){
-      $(this).removeClass('typed').addClass('untyped');
-        if (key < progressServer){
-          $(charDiv).removeClass('untyped').addClass('typed');
-        }
-      });
-    }
+  function buildText(){
+    $("#visuText").empty();
+    var textArray = text.split('');
+    $.each(textArray, function(index,char){
+      var charDiv = $('<span>').addClass("singleChar untyped").html(char).appendTo($("#visuText"));
+    });
+  }
 
 
-    // auto scroll down
-    window.setInterval(function() {
+  function actuVisu(){
+    $('.singleChar').each(function(key, charDiv){
+    $(this).removeClass('typed').addClass('untyped');
+      if (key < progressServer){
+        $(charDiv).removeClass('untyped').addClass('typed');
+      }
+    });
+    autoscrolldown();
+  }
+
+  var visuHeight;
+
+  function autoscrolldown(){
+    var documentHeight = document.body.scrollHeight;
+    var newvisuHeight = $("#visuText").height();
+    if (newvisuHeight != visuHeight){
       $('body').animate({scrollTop: document.body.scrollHeight}, 1000);
-    }, 6000);
+      // $('body').scrollTop(document.body.scrollHeight);
+      visuHeight = newvisuHeight;
+    }
+  }
 
 
+  ///////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////
+  //                     SOCKET
+  ///////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////
 
-    ///////////////////////////////////////////////////////
-    ///////////////////////////////////////////////////////
-    //                     SOCKET
-    ///////////////////////////////////////////////////////
-    ///////////////////////////////////////////////////////
+  var IO_PORT = 8089;
+  url = 'http://'+document.location.hostname+':'+IO_PORT;
+  var socket = io(url);
+  var initApp = true;
 
-    var IO_PORT = 8089;
-    url = 'http://'+document.location.hostname+':'+IO_PORT;
-    var socket = io(url);
-    var initApp = true;
+  socket.on('connect', function () {
+    console.log('Connected to Server: '+url);
+  });
 
-    socket.on('connect', function () {
-      console.log('Connected to Server: '+url);
-    });
+  socket.on('fulltext', function (data) {
+    text = data;
+    buildText();
+    initApp = true;
+  });
 
-    socket.on('fulltext', function (data) {
-      text = data;
-      buildText();
-      initApp = true;
-    });
+  socket.on('progress', function (data) {
+    progressServer = data;
+    actuVisu();
 
-    socket.on('progress', function (data) {
-      progressServer = data;
-      actuVisu();
-
-    });
+  });
 
 
 
