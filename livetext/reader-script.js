5c4a63e5337a61ce6bd942ca6f69c8b95ace4eec
Jeremie <jeremie@hemisphere-project.com>
2015-11-24 16:53:45 +0100

    reader ok

--- livetext/reader-script.js
+++ livetext/reader-script.js
@@ -2,6 +2,60 @@
 $(function() {
 
 
+    var text='temp text ceci est un texte essai';
+    var progressServer = 0;
+
+
+    buildText();
+
+    function buildText(){
+      $("#visuText").empty();
+      var textArray = text.split('');
+      $.each(textArray, function(index,char){
+        var charDiv = $('<span>').addClass("singleChar untyped").html(char).appendTo($("#visuText"));
+      });
+    }
+
+
+    function actuVisu(){
+      $('.singleChar').each(function(key, charDiv){
+      $(this).removeClass('typed').addClass('untyped');
+        if (key < progressServer){
+          $(charDiv).removeClass('untyped').addClass('typed');
+        }
+      });
+    }
+
+
+
+    ///////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
+    //                     SOCKET
+    ///////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
+
+    var IO_PORT = 8089;
+    url = 'http://'+document.location.hostname+':'+IO_PORT;
+    var socket = io(url);
+    var initApp = true;
+
+    socket.on('connect', function () {
+      console.log('Connected to Server: '+url);
+    });
+
+    socket.on('fulltext', function (data) {
+      text = data;
+      buildText();
+      initApp = true;
+    });
+
+    socket.on('progress', function (data) {
+      progressServer = data;
+      actuVisu();
+
+    });
+
+
 
 
 });
