bb2982c6ab91128922f1bd2c7402d8fde2091763
Jeremie <jeremie@hemisphere-project.com>
2015-11-24 14:34:24 +0100

    sync

--- writer/script.js
+++ writer/script.js
@@ -1,36 +1,61 @@
 
-  $(function() {
+$(function() {
 
 
-    var text='Depuis le lancement de Framapad en 2011, son succès ne s’est pas démenti. Pour preuve, nos serveurs s’écroulent ! C’est pourquoi, depuis maintenant près de 2 ans, nous multiplions les instances d’Etherpad afin de garder un service réactif. Les soucis à répétition sur notre infrastructure qui ont eu lieu pendant les deux premiers mois de 2015 ont monopolisé notre attention. Du coup, la dernière instance de Framapad, lite5, s’est retrouvée à héberger près de 90 000 pads ! Depuis son lancement à la mi-octobre, cela fait quand même 18 000 pads par mois, soit 600 nouveaux pads par jour, ou 25 pads par heure !';
-    var progressText = 0;
+  var text='temp text';
+  var progressText = 0;
 
+
+  function buildText(){
     var textArray = text.split('');
-
     $.each(textArray, function(index,char){
       var charDiv = $('<span>').addClass("singleChar untyped").html(char).appendTo($("#visuText"));
     });
+  }
 
 
+  document.onkeydown = function(e){
+    progressText ++;
+    actuVisu();
+  }
 
-    document.onkeydown = function(e){
-      progressText ++;
-      actuVisu();
-    }
+  function actuVisu(){
+    $('.singleChar').each(function(key, charDiv){
+      if (key < progressText){
+        $(charDiv).removeClass('untyped').addClass('typed');
+      }
+    });
+  }
 
-    function actuVisu(){
-      $('.singleChar').each(function(key, charDiv){
-        if (key < progressText){
-          $(charDiv).removeClass('untyped').addClass('typed');
-        }
-      });
-    }
+  //attendre arrivée d'un progress pour afficher texte
+
+  // stocker progress recu dans un coin. quand on s'arrete de taper, comparer
 
 
-    // keypressed
-    //
-    // fulltext
-    // progress
+   socket.emit('keypressed', true);
+
+
+  ///////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////
+  //                     SOCKET
+  ///////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////
+
+  var IO_PORT = 8089;
+  url = 'http://'+document.location.hostname+':'+IO_PORT;
+  var socket = io(url);
+
+  socket.on('connect', function () {
+    console.log('Connected to Server: '+url);
+  });
+
+  socket.on('fulltext', function (data) {
+    text = data.fulltext;
+  });
+
+  socket.on('progress', function (data) {
+
+  });
 
 
 });
